62bd7c6837657ff2022bc1807034c8e8daef54b9
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-09-30 15:41:26 +0100

    Added AgeInHumanYears override abstract

--- AnimalYears.Domain/Library.cs
+++ AnimalYears.Domain/Library.cs
@@ -4,14 +4,16 @@
     {
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public abstract int AgeInHumanYears();
     }
 
     public class Cat : Animal
     {
-
+        public override int AgeInHumanYears() { return Age * 5; }    
     }
     public class Dog : Animal
     {
-
+        public override int AgeInHumanYears() { return Age * 7; }
     }
 }
