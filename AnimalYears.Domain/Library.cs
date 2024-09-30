316cf3f84eda3856d92621c4491c58428348c897
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-09-30 15:19:52 +0100

    renamed files

--- AnimalYears.Domain/Library.cs
+++ AnimalYears.Domain/Library.cs
@@ -1,6 +1,6 @@
 ﻿namespace AnimalYears.Domain
 {
-    public class Class1
+    public class Library
     {
 
     }
--- AnimalYearsTests/UnitTest1.cs
+++ AnimalYearsTests/UnitTest1.cs
@@ -1,6 +1,6 @@
 namespace AnimalYearsTests
 {
-    public class UnitTest1
+    public class AnimalYearsUnitTests
     {
         [Fact]
         public void Test1()
